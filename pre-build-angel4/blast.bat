0619241653410c5c807cc5082e771b4abb20c753
Frank Dols <frank.dols@synopsys.com>
2012-11-14 14:04:20 +0100

    ARC: grand rename action into self explanatory names.
    Signed-off-by: Frank Dols <frank.dols@synopsys.com>

--- pre-build-angel4/blast.bat
+++
@@ -1,3 +0,0 @@
-echo off
-
-scac -hard -off=flush_pipe -DLL=C:\AshlingOpellaXDforARC\opxdarc -blast=C:\000-use-this-folder-for-your-tests-000\frank\angel4\A7_80MHz_64bit_I32k_D32k_AA4.xbf -memxfersize=0x8000 -prop=jtag_frequency=1 -on=jtag_optimise -prop=auto_address=1 -prop=gclk0=72 -prop=gclk=80 -on=reload_readonly -mmu C:\000-use-this-folder-for-your-tests-000\frank\angel4\vmlinux
